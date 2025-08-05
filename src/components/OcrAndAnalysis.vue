be02a0dd7970b4dba6fd2f08cae7e21b590002fe
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-05 14:07:02 +0200

    ADD main parent component

---
+++ src/components/OcrAndAnalysis.vue
@@ -0,0 +1,24 @@
+<template>
+  <TextConverter @text-extracted="handleTextExtracted" />
+  <TextAnalyzer :text="extractedText" />
+
+  <p v-if="text">
+    <strong>Testo estratto in genitore: </strong>{{ extractedText }}
+  </p>
+  <!-- DEBUG -->
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+
+const extractedText = ref("");
+
+watch(extractedText, (val) => {
+  console.log("extractedText cambiato a:", val);
+});
+
+function handleTextExtracted(text) {
+  console.log("Evento ricevuto con testo:", text);
+  extractedText.value = text;
+}
+</script>
