fd4031ccd14eddef1784ce8a5440b4d45f3c9f29
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-05 10:18:27 +0200

    MVP TextConverter component w/ tesseract

--- src/components/TextConverter.vue
+++ src/components/TextConverter.vue
@@ -1,7 +1,26 @@
 <template>
-  <h2>Ciao mamma</h2>
+  <div>
+    <h2>OCR Result</h2>
+    <p v-if="text">{{ text }}</p>
+    <p v-else>Caricamento in corso...</p>
+  </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref, onMounted } from "vue";
+import Tesseract from "tesseract.js";
+
+const text = ref("");
+
+onMounted(async () => {
+  const result = await Tesseract.recognize(
+    "./book-cover.jpg",
+    "ita", // Lingua italiana
+    {
+      logger: (m) => console.log(m),
+    }
+  );
+
+  text.value = result.data.text;
+});
 </script>
