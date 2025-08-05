c8266fb3916565af86bef984b022cc437a6e4116
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-05 14:06:51 +0200

    ADD components

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import TextConverter from "./components/TextConverter.vue";
+import TextAnalyzer from "./components/TextAnalyzer.vue";
+import OcrAndAnalysis from "./components/OcrAndAnalysis.vue";
 
 const app = createApp(App);
 
-app.component("text-converter", TextConverter);
+app.component("OcrAndAnalysis", OcrAndAnalysis);
+app.component("TextConverter", TextConverter);
+app.component("TextAnalyzer", TextAnalyzer);
 
 app.mount("#app");
