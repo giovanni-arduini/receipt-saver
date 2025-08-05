60a99ebb83bafc3d54781281751a2bfb734f9583
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-05 11:04:21 +0200

    ADD file upload

--- src/components/TextConverter.vue
+++ src/components/TextConverter.vue
@@ -2,25 +2,52 @@
   <div>
     <h2>OCR Result</h2>
     <p v-if="text">{{ text }}</p>
-    <p v-else>Caricamento in corso...</p>
+    <p v-else-if="loading">Caricamento in corso...</p>
+    <p v-else>Carica un file per iniziare</p>
+  </div>
+
+  <div>
+    <label for="uploaded"> Upload a JPG, PDF or PNG file: </label>
+    <input
+      id="uploaded"
+      type="file"
+      accept="image/png, image/jpeg"
+      @change="handleFileUpload"
+    />
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 import Tesseract from "tesseract.js";
 
 const text = ref("");
+const loading = ref(false);
 
-onMounted(async () => {
-  const result = await Tesseract.recognize(
-    "./book-cover.jpg",
-    "ita", // Lingua italiana
-    {
-      logger: (m) => console.log(m),
+const handleFileUpload = async (e) => {
+  const file = e.target.files[0];
+  if (!file) return;
+
+  const reader = new FileReader();
+
+  reader.onload = async () => {
+    loading.value = true;
+    text.value = "";
+
+    try {
+      const result = await Tesseract.recognize(reader.result, "ita", {
+        logger: (message) => console.log("[OCR progress]", message),
+      });
+
+      text.value = result.data.text;
+    } catch (error) {
+      console.error("Errore durante l'OCR:", error);
+      text.value = "Errore durante l'OCR.";
+    } finally {
+      loading.value = false;
     }
-  );
+  };
 
-  text.value = result.data.text;
-});
+  reader.readAsDataURL(file);
+};
 </script>
