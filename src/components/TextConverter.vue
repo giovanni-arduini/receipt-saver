027f5face5f1b9165b98cc331ab2909ca638e3c4
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-05 14:07:47 +0200

    ADD emit from TextConverter

--- src/components/TextConverter.vue
+++ src/components/TextConverter.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h2>OCR Result</h2>
-    <p v-if="text">{{ text }}</p>
+    <h2>Salva la tua fattura!</h2>
+    <p v-if="text"><strong>Il testo nell'immagine è: </strong>{{ text }}</p>
     <p v-else-if="loading">Caricamento in corso...</p>
     <p v-else>Carica un file per iniziare</p>
   </div>
@@ -18,13 +18,15 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, defineEmits } from "vue";
 import Tesseract from "tesseract.js";
 
 const text = ref("");
 const loading = ref(false);
+const emit = defineEmits(["text-extracted"]);
 
 const handleFileUpload = async (e) => {
+  console.log("File caricato, gestisco l'evento change");
   const file = e.target.files[0];
   if (!file) return;
 
@@ -35,13 +37,13 @@ const handleFileUpload = async (e) => {
     text.value = "";
 
     try {
-      const result = await Tesseract.recognize(reader.result, "ita", {
-        logger: (message) => console.log("[OCR progress]", message),
-      });
+      const result = await Tesseract.recognize(reader.result, "ita", {});
 
       text.value = result.data.text;
+      console.log("Sto per emettere l'evento con testo:", result.data.text);
+      emit("text-extracted", result.data.text);
     } catch (error) {
-      console.error("Errore durante l'OCR:", error);
+      console.error("Errore", error);
       text.value = "Errore durante l'OCR.";
     } finally {
       loading.value = false;
