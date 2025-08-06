c4e7b60b28872336166f9b54d89d5c10162551c3
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-06 19:43:19 +0200

    ADD SideTab component

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from "./App.vue";
 import TextConverter from "./components/TextConverter.vue";
 import TextAnalyzer from "./components/TextAnalyzer.vue";
 import OcrAndAnalysis from "./components/OcrAndAnalysis.vue";
+import SideTab from "./components/SideTab.vue";
 
 const app = createApp(App);
 
 app.component("OcrAndAnalysis", OcrAndAnalysis);
 app.component("TextConverter", TextConverter);
 app.component("TextAnalyzer", TextAnalyzer);
+app.component("SideTab", SideTab);
 
 app.mount("#app");
