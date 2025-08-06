5912758bfbd0d8f7c5056b6c2c050d0c15eb9692
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-06 19:43:29 +0200

    ADD SideTab vue component

---
+++ src/components/SideTab.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="side-tab">
+    <div id="quick-add"></div>
+    <div id="list-container"></div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.side-tab {
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  min-width: 15%;
+  background-color: rgb(249, 241, 241);
+}
+</style>
