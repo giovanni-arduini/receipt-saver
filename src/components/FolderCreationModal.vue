7a7ea4a06d2316091876a1bb897236246dde66a9
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-09 11:29:22 +0200

    REFACTOR dialog element for FolderCreationModal

--- src/components/FolderCreationModal.vue
+++ src/components/FolderCreationModal.vue
@@ -1,15 +1,16 @@
 <template>
-  <form @submit.prevent="createNewFolder">
-    <input v-model="newFolderName" type="text" />
-    <input v-model="newFolderDate" type="date" />
-    <select v-model="newFolderCategory">
-      <option value="Fatture">Fatture</option>
-      <option value="Ricette">Ricette</option>
-    </select>
-
-    <button>Crea</button>
-    <button>Chiudi</button>
-  </form>
+  <dialog ref="dialogRef" open>
+    <form method="dialog" @submit.prevent="createNewFolder">
+      <input v-model="newFolderName" type="text" placeholder="Nome cartella" />
+      <input v-model="newFolderDate" type="date" />
+      <select v-model="newFolderCategory">
+        <option value="Fatture">Fatture</option>
+        <option value="Ricette">Ricette</option>
+      </select>
+      <button type="submit">Crea</button>
+      <button type="button" @click="closeModal">Chiudi</button>
+    </form>
+  </dialog>
 </template>
 
 <script setup>
@@ -18,6 +19,7 @@ const newFolderName = ref("");
 const newFolderDate = ref(new Date());
 console.log(newFolderDate);
 const newFolderCategory = ref("Fatture");
+const dialogRef = ref(null);
 
 const emit = defineEmits(["add-folder", "close-modal"]);
 
@@ -33,5 +35,10 @@ function createNewFolder() {
     newFolderCategory.value
   );
   newFolderName.value = "";
+  emit("close-modal");
+}
+
+function closeModal() {
+  emit("close-modal");
 }
 </script>
