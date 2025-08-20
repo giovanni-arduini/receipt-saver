71ef5b68b1e59c36e9c9a5e58221f8d570adb852
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-20 12:44:07 +0200

    ADD FileDetail component

---
+++ src/components/FileDetail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>{{}}</h1>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+// import { useFiles } from "@/useFiles";
+// const { showFile } = useFiles();
+
+// onMounted(showFile(id));
+</script>
