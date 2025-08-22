335143101a166cb6a173837c19a68ea6c8141047
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-22 12:02:13 +0200

    FileDetail component w/ basic props

--- src/components/FileDetail.vue
+++ src/components/FileDetail.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
-    <h1>{{}}</h1>
+  <div class="row-start-3 col-start-2 row-end-6 col-end-6 bg-green-200">
+    <h1>{{ file.name }}</h1>
+    <h2>{{ file.category }}</h2>
+    <p>{{}}</p>
+    <button @click="hideDetail">Back</button>
   </div>
 </template>
 
 <script setup>
-import { onMounted } from "vue";
-// import { useFiles } from "@/useFiles";
-// const { showFile } = useFiles();
+import { defineProps } from "vue";
 
-// onMounted(showFile(id));
+defineProps({
+  file: Object,
+  hideDetail: Function,
+});
 </script>
