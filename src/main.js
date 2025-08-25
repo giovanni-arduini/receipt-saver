0cc8b3372034bf5b1e01f473b3b7a0d99002e41d
giovanni-arduini <gionnibigudd@gmail.com>
2025-08-25 22:19:53 +0200

    ADD font awesome components

--- src/main.js
+++ src/main.js
@@ -8,9 +8,15 @@ import MainList from "./components/MainList.vue";
 import FileDetail from "./components/FileDetail.vue";
 import SummaryGraph from "./components/SummaryGraph.vue";
 import VueApexCharts from "vue3-apexcharts";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import "./assets/tailwind.css";
 import ModificationsModal from "./components/ModificationsModal.vue";
+
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faTrashCan);
 
 const app = createApp(App);
 
@@ -23,5 +29,6 @@ app.component("MainList", MainList);
 app.component("SummaryGraph", SummaryGraph);
 app.component("FileDetail", FileDetail);
 app.component("ModificationsModal", ModificationsModal);
+app.component("FontAwesomeIcon", FontAwesomeIcon);
 
 app.mount("#app");
